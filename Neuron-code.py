8b7971fb48d94fea4fab09a4ea97d219a7ff6e04
Jonathan Chao <jonathan.chao7@gmail.com>
2020-12-26 08:00:12 -0800

    Update Neuron-code.py

--- Neuron-code.py
+++ Neuron-code.py
@@ -31,6 +31,26 @@ output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] 
 
 print(output)
 
+
+#using for loops
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2.0, 3.0, 0.5]
+
+layer_outputs = [] #output of current layer
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0 #output of given neuron
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+    
+print(layer_outputs)
+
 '''
 notes:
 Inputs - values that you have that you are trying to track or could be an input layer
