25c016b80f7d05277489098d5377a58478fed24d
Jonathan Chao <jonathan.chao7@gmail.com>
2020-12-26 07:41:24 -0800

    Create Neuron-code.py

---
+++ Neuron-code.py
@@ -0,0 +1,39 @@
+#modeling 1 neuron with 4 inputs from hidden layer
+
+# every input has its own weights
+inputs = [1, 2, 3, 2.5]
+
+#weights, same number of neuron
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
+
+print(output)
+
+
+
+#modeling 3 neurons with 4 inputs from hidden layer
+inputs = [1, 2, 3, 2.5]
+
+#weights, same number of neuron
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
+          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
+          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
+
+print(output)
+
+'''
+notes:
+Inputs - values that you have that you are trying to track or could be an input layer
+Weights - lines between nodes(circles)
+Biases - a single neuron, calculated as an output
+'''
